97f812ce9dd05096f845f687d3818d05adb924bf
Franco Rod <81878824+Francorod@users.noreply.github.com>
2022-03-14 18:59:20 -0400

    Passing data between TableView.

--- TableViewPassdata/ViewController.swift
+++ TableViewPassdata/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Category {
+    let title: String
+    let items: [String]
+}
 
+class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let data: [Category] = [
+    Category(title: "Fruits", items: ["Apple","Banana","Pinaple"]),
+    Category(title: "Car Models", items: ["BMW","Audi","Ferrati"]),
+    Category(title: "Apple Devices", items: ["Mac","Ipad","Ipod"]),
+    Category(title: "Cities", items: ["LA","SF","NY"])
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
 
-
+    }
 }
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let category = data[indexPath.row]
+        
+        let vc = ListViewController(items: category.items)
+        vc.title = category.title
+        navigationController?.pushViewController(vc, animated: true)
+
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].title
+        return cell
+    }
+}
+
+
 
